d6763e9a1365eff2f41ca625530c48f04db7e3be
julien <deniaudjulien0@gmail.com>
2022-07-02 20:22:26 +0200

    select opérationnelle

--- la programation évènementielle/script.js
+++ la programation évènementielle/script.js
@@ -12,11 +12,38 @@ console.log(userNumber.length);
 console.log(userNumber);
 console.log(h1);
 
+function displayPseudo1(){
+  const pseudo1 = document.getElementById('pseudo1');
+  const displayPseudo1 = pseudo1.value;    
+  document.getElementById("displayPseudo").innerText = displayPseudo1;
+  console.log("utilisateur 1  pseudo : " + displayPseudo1);   
+}
+
+function displayMessage1(){
+  const message1 = document.getElementById('message1');
+  const displayMessage1 = message1.value;
+  document.getElementById("displayMessage").innerText = displayMessage1;
+  console.log("utilisateur 1 message : " + displayMessage1 );
+}
+
+function displayPseudo2(){
+  const pseudo2 = document.getElementById('pseudo2');
+  const displayPseudo2 = pseudo2.value;
+  document.getElementById("displayPseudo2").innerText = displayPseudo2;
+  console.log("utilisateur 2  pseudo : " + displayPseudo2);
+}
+
+function displayMessage2(){
+  const message2 = document.getElementById('message2');
+  const displayMessage2 = message2.value;
+  document.getElementById("displayMessage2").innerText = displayMessage2;
+  console.log("utilisateur 2 message : " + displayMessage2 );
+}
+
 //appel au dom
 soumettre2.addEventListener('click', displayMessage1(),displayMessage2() , displayPseudo1(), displayPseudo2());
 
 console.log('JavaScript bien chargé');
-
 
 /*             partie du select           */
 /******************************************/
@@ -35,35 +62,10 @@ selectElem.addEventListener('change', function() {
 console.log(index);
 
 console.log(selectElem);
+
 if (index > 1){
   
-  function displayPseudo1(){
-    const pseudo1 = document.getElementById('pseudo1');
-    const displayPseudo1 = pseudo1.value;    
-    document.getElementById("displayPseudo").innerText = displayPseudo1;
-    console.log("utilisateur 1  pseudo : " + displayPseudo1);   
-  }
-
-  function displayMessage1(){
-    const message1 = document.getElementById('message1');
-    const displayMessage1 = message1.value;
-    document.getElementById("displayMessage").innerText = displayMessage1;
-    console.log("utilisateur 1 message : " + displayMessage1 );
-  }
 
 }else{
-  function displayPseudo2(){
-    const pseudo2 = document.getElementById('pseudo2');
-    const displayPseudo2 = pseudo2.value;
-    document.getElementById("displayPseudo2").innerText = displayPseudo2;
-    console.log("utilisateur 2  pseudo : " + displayPseudo2);
-  }
-
-  function displayMessage2(){
-    const message2 = document.getElementById('message2');
-    const displayMessage2 = message2.value;
-    document.getElementById("displayMessage2").innerText = displayMessage2;
-    console.log("utilisateur 2 message : " + displayMessage2 );
-  }
 
 }
